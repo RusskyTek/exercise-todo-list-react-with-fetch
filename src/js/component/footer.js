38bb9226b4a9b7893464147085c3d098ed1b3b9d
PitiTheo <julio.vilaboa@yandex.com>
2021-04-10 19:03:03 +0000

    empezando trabajo

---
+++ src/js/component/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<div
+			className="text-center p-3"
+			style={{ backgroundColor: "lightblue", color: "grey" }}>
+			<img
+				className="imageSize pb-5"
+				src="https://4geeksacademy.com/static/59abdc351548ee6640c495ffbb13967c/e22c9/4G_logo_negro.png"
+			/>
+			<h5> Hecho por Julio Vilaboa-4Geeks Academy </h5>
+		</div>
+	);
+};
